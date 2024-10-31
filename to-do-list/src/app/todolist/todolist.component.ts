6be8f7a7487c41cb45ee58c82c8c38c19c63154a
Souhabeda <souhaab256@gmail.com>
2024-11-01 00:21:08 +0100

    to-do-list

---
+++ to-do-list/src/app/todolist/todolist.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Task {
+  taskName: string;
+  isCompleted: boolean;
+}
+
+@Component({
+  selector: 'app-todolist',
+  templateUrl: './todolist.component.html',
+  styleUrls: ['./todolist.component.css']
+})
+export class TodolistComponent implements OnInit {
+  taskArray: Task[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    this.taskArray.push({
+      taskName: form.controls['task'].value,
+      isCompleted: false
+    });
+    form.reset();
+  }
+
+  onDelete(index: number) {
+    this.taskArray.splice(index, 1);
+  }
+
+  onCheck(index: number) {
+    this.taskArray[index].isCompleted = !this.taskArray[index].isCompleted;
+  }
+}
